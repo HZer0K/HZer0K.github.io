d39797f8991548a5f33b25d18087d3166f750484
xiaokang <2395711943@qq.com>
2025-07-24 21:24:49 +0800

    Site updated: 2025-07-24 21:24:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/09/hello-world/","2025/07/13/编译FFmpeg/","2025/07/09/mediacodec/"];function toRandomPost(){
+var posts=["2025/07/09/mediacodec/","2025/07/09/hello-world/","2025/07/13/编译FFmpeg/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
