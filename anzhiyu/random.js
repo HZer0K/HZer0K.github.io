965eb822a302e683871920ba39b954785f11d0cf
xiaokang <2395711943@qq.com>
2026-05-17 16:26:00 +0800

    Site updated: 2026-05-17 16:25:57

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/13/编译FFmpeg/","2025/07/09/Hexo简单使用/","2025/07/24/喜欢的文章/","2026/05/10/IJKPLAYER 集成大模型架构思考/","2026/05/10/IJKPLAYER 通用AI推理框架架构设计/","2026/05/11/IJKPLAYER 集成大模型实现（一）/"];function toRandomPost(){
+var posts=["2025/07/09/Hexo简单使用/","2026/05/11/IJKPLAYER 集成大模型实现（一）/","2026/05/10/IJKPLAYER 集成大模型架构思考/","2025/07/13/编译FFmpeg/","2026/05/10/IJKPLAYER 通用AI推理框架架构设计/","2025/07/24/喜欢的文章/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
